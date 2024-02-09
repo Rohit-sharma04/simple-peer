af76f9e0e3c5f5b76e3e80ed25d6ed275b6c6a63
rohit <rohitsharma4102002@gmail.com>
2024-02-09 13:22:23 +0530

    upadted origin url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import io from "socket.io-client"
 import "./App.css"
 
 
-const socket = io.connect('http://localhost:5001')
+const socket = io.connect('https://simple-peer-backend.onrender.com')
 function App() {
   const [me, setMe] = useState("")
   const [stream, setStream] = useState()
