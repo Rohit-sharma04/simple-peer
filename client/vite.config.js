3851af1174f24506b5e1dc4fde609f1e1063e08a
rohit <rohitsharma4102002@gmail.com>
2024-02-09 13:00:05 +0530

    first commit

---
+++ client/vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import nodePolyfills from "rollup-plugin-polyfill-node";
+import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    global: {}
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Enable esbuild polyfill plugins
+      plugins: [nodeModulesPolyfillPlugin()],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+  },
+  
+})
